21841a99d6213f17cd31617e09e8e632598b52f6
keeYuc <52500690@qq.com>
2021-04-15 15:37:26 +0800

    1

---
+++ test/test_init/main.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main(){
+	var a bool
+	fmt.Println(a)
+	if !a{
+		fmt.Println("yes")
+	a=true
+	}
+	fmt.Println(a)
+	}
+
--- tool/tool.go
+++ tool/tool.go
@@ -4,27 +4,33 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
-var GlobalRedis redis.Pool
+var (
+	GlobalRedis redis.Pool
+	redis_init  bool
+)
 
-//mysql 配置
-
-const MysqlName = "root"
-const MysqlPwd = "Fucker123"
-const MysqlAddr = "127.0.0.1"
-const MysqlPort = "3306"
-const MysqlDB = "fuckhome"
-
-//redis 配置
-const RedisAddr = "127.0.0.1:6379"
+const (
+	//mysql 配置
+	MysqlName = "root"
+	MysqlPwd  = "Fucker123"
+	MysqlAddr = "127.0.0.1"
+	MysqlPort = "3306"
+	MysqlDB   = "IhomeData"
+	//redis 配置
+	RedisAddr = "127.0.0.1:6379"
+)
 
 //接口部分
 func init() {
-	GlobalRedis = redis.Pool{
-		MaxIdle:     20,
-		MaxActive:   50,
-		IdleTimeout: 60 * 5,
-		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", RedisAddr)
-		},
+	if !redis_init {
+		GlobalRedis = redis.Pool{
+			MaxIdle:     20,
+			MaxActive:   50,
+			IdleTimeout: 60 * 5,
+			Dial: func() (redis.Conn, error) {
+				return redis.Dial("tcp", RedisAddr)
+			},
+		}
+		redis_init = true
 	}
 }

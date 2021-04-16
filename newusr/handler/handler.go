9a7ab5d2d2c2810b36fc5163d15e1859ef15b038
keeYuc <52500690@qq.com>
2021-04-16 14:14:59 +0800

    gx

--- newusr/handler/handler.go
+++ newusr/handler/handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"keeyu/message"
+	"keeyu/tool"
 )
 
 type NewUsr struct {
@@ -15,9 +16,14 @@ func (s *NewUsr) Call(ctx context.Context, in *message.Usr) (*message.Usr, error
 	if phone == "" || pwd == "" || smscode == "" {
 		return in, fmt.Errorf("Usr信息不能有空的")
 	}
+	save_mysql(ctx, phone, pwd, smscode)
 	return in, nil
 }
 
-func save_mysql(phone, pwd, sms string) {
-
+func save_mysql(ctx context.Context, phone, pwd, sms string) error {
+	// conn, err := tool.GlobalMysql.Conn(ctx)
+	conn, _ := tool.GlobalMysql.Conn(ctx)
+	// if err != nil {
+	// 	return err
+	// }
 }
